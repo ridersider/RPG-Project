e2982ff279728ad5a93b02602700421d5ea935d2
Fabio Geisser <fabio.geisser@gmail.com>
2025-11-26 16:58:26 +0100

    added sprites and animation script

--- Assets/Scripts/Player/PlayerController2D.cs
+++ Assets/Scripts/Player/PlayerController2D.cs
@@ -3,15 +3,19 @@ using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Health))]
+[RequireComponent(typeof(SpriteAnimator))]
 public class PlayerController2D : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    private float _moveSpeed = 5f;
 
     private Rigidbody2D _rb;
     private Health _health;
+    private SpriteAnimator _animator;
 
     private Vector2 _moveInput;
     private Vector2 _lastMoveDir = Vector2.right;
+    
+    private bool _isMoving;
     
     public PlayerData playerData;
 
@@ -19,19 +23,20 @@ public class PlayerController2D : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _health = GetComponent<Health>();
+        _animator = GetComponent<SpriteAnimator>();
     }
 
     private void Start()
     {
         ApplyCharacterDefinition();
+        _animator.Play(PlayerAnimState.Idle);
     }
 
     public void ApplyCharacterDefinition()
     {
         if (playerData == null) return;
 
-        moveSpeed = playerData.moveSpeed;
-
+        _moveSpeed = playerData.moveSpeed;
         _health.Initialize(playerData.maxHealth);
     }
 
@@ -44,12 +49,19 @@ public class PlayerController2D : MonoBehaviour
         if (_moveInput.sqrMagnitude > 0.01f)
         {
             _lastMoveDir = _moveInput;
+            
+            _animator.SetFacingDirection(_lastMoveDir);
+            _animator.Play(PlayerAnimState.Run);
+        }
+        else
+        {
+            _animator.Play(PlayerAnimState.Idle);
         }
     }
 
     private void FixedUpdate()
     {
-        _rb.linearVelocity = _moveInput * moveSpeed;
+        _rb.linearVelocity = _moveInput * _moveSpeed;
     }
 
     public Vector2 GetAimDirection()
